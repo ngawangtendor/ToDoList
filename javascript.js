004875254c9acfd036703d401e30a5c06cfbdfad
ngawangtendor <nicolathomas1999@gmail.com>
2024-06-09 23:57:26 +0200

    DOM

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const buttonSubmit = document.querySelector(".buttonSubmit");
+    buttonSubmit.addEventListener("click", function() {
+        const lada = document.querySelector(".s")
+        lada.value = "";
+    });
+});
