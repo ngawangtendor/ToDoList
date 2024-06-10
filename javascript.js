c0a0fac29c6b4b4698e1e527807619dfbe6bc8f2
ngawangtendor <nicolathomas1999@gmail.com>
2024-06-10 18:50:55 +0200

    finition

--- javascript.js
+++ javascript.js
@@ -20,13 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
         // Ajoute la valeur de l'input au paragraphe
         newParagraph.innerHTML = inputs.value;
         
-        // Crée un nouvel élément d'input de type checkbox
-        const newCheckbox = document.createElement("input");
-        newCheckbox.type = "checkbox";
-        
-        // Ajoute la checkbox au paragraphe
-        newParagraph.appendChild(newCheckbox);
-        
         // Insère le nouveau paragraphe après le paragraphe existant
         paragraph.insertAdjacentElement('beforeend', newParagraph);
 
